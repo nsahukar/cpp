3a8d4e0c71b5287a57f7acd5335871694a8705e8
Nikhil Sahukar <nsahukar@gmail.com>
2021-08-25 01:18:49 +0530

    Made the map move on AWSD keys

--- gamedev/classy-clash/main.cpp
+++ gamedev/classy-clash/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main() {
 	// window dimensions
@@ -12,6 +13,8 @@ int main() {
 	Texture2D map = LoadTexture("nature_tileset/OpenWorldMap.png");
 	// map position
 	Vector2 mapPos{0.0, 0.0};
+	// map speed
+	const float mapVel = 4.0;
 
 	SetTargetFPS(60);
 	while (!WindowShouldClose()) {
@@ -19,6 +22,17 @@ int main() {
 		BeginDrawing();
 		ClearBackground(WHITE);
 
+		// moving map
+		Vector2 direction{};
+		if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+		if (IsKeyDown(KEY_D)) direction.x += 1.0;
+		if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+		if (IsKeyDown(KEY_S)) direction.y += 1.0;
+		// optimizing direction
+		if (Vector2Length(direction) != 0.0) {
+			mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), mapVel));
+		}
+		
 		// draw map
 		DrawTextureEx(map, mapPos, 0.0, 1.0, WHITE);
 
